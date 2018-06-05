4c58615892e3c95e3add314ba15f73e2f3a2623e
Fernando <fernand-o.a@hotmail.com>
2018-06-05 19:08:32 +0100

    used redux

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,12 +1,49 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { decrementCounter, incrementCounter } from '../actions';
+import { AppState } from '../reducers';
 
-export default class Header extends React.Component<{}, {}> {
+export interface HeaderProps {
+    counter: number;
+    incrementCounter?: (value: number) => {};
+    decrementCounter?: (value: number) => {};
+}
+
+class Header extends React.Component<HeaderProps, {}> {
+
+    constructor(props) {
+        super(props);
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+
+    increment() {
+        this.props.incrementCounter(2);
+    }
+
+    decrement() {
+        this.props.decrementCounter(2);
+    }
+
     render() {
         return <ul>
-            <li><Link to='/home'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='/movies'>Movies</Link></li>
+            <li>Number of movies:  {this.props.counter}</li>
+            <li>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
+            </li>
         </ul>;
     }
 }
+
+const mapStateToProps = (state: AppState) => {
+    return { counter: state.counter.value };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    incrementCounter: (value: number) => dispatch(incrementCounter(value)),
+    decrementCounter: (value: number) => dispatch(decrementCounter(value))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
